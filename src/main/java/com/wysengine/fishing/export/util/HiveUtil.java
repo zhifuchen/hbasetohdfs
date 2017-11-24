81c4ad15ad459e81b0273e1ab1cb233d909e9896
zhifuchen <zhifuchen@qq.com>
2017-11-24 14:53:43 +0800

    修改

--- src/main/java/com/wysengine/fishing/export/util/HiveUtil.java
+++ src/main/java/com/wysengine/fishing/export/util/HiveUtil.java
@@ -16,16 +16,18 @@ public class HiveUtil {
     private static final String driverName = "org.apache.hive.jdbc.HiveDriver";
     private static final String hdfsUrl = PropertyUtil.getProperty("hdfs.default");
     private static final String outputPath = PropertyUtil.getProperty("output.path");
+    private static final String avroTablePrefix = "avro_";
+    private static final String parquetTablePrefix = "parquet_";
 
     public static void createTable(String tableName) throws SQLException, ClassNotFoundException {
         Class.forName(driverName);
         try (Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000", "hdfs", "");
              Statement stmt = con.createStatement()) {
             stmt.execute("DROP TABLE IF EXISTS " + tableName);
-            String avroTable = "avro_" + tableName;
+            String avroTable = avroTablePrefix + tableName;
             String avroSchemaUrl = hdfsUrl +  outputPath + "schema/" + tableName + "/schema.avsc";
             stmt.execute("CREATE TABLE " + avroTable + " ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.avro.AvroSerDe' STORED AS AVRO TBLPROPERTIES ('avro.schema.url'='" + avroSchemaUrl + "')");
-            String parquetTable = "parquet_" + tableName;
+            String parquetTable =  parquetTablePrefix + tableName;
             String parquetUrl = hdfsUrl + outputPath + "parquet/" + tableName;
             stmt.execute("CREATE EXTERNAL TABLE " + parquetTable + " LIKE " + avroTable + " STORED AS PARQUET LOCATION '"+ parquetUrl +"'");
         }
